1607365383e5dc787406dc9e52799bea279ef633
Dmity <Ageev_Dmity_2016@mail.ru>
2020-10-05 19:37:47 +0300

    добавляет компонент Room

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main";
 import Login from "../login/login";
-import Favorites from "../favorites/favorites"
+import Favorites from "../favorites/favorites";
+import Room from "../room/room";
 
 const App = (props) => {
   const {offersCount} = props;
@@ -19,6 +20,9 @@ const App = (props) => {
         </Route>
         <Route exact path="/favorites">
           <Favorites/>
+        </Route>
+        <Route exact path="/offer/:id">
+          <Room/>
         </Route>
       </Switch>
     </BrowserRouter>
